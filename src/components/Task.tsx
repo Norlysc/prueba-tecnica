e0d8d352ab57554a239c611ce0577a401c5a769e
Norlys Castañeda <castaneda30@hotmail.com>
2024-05-25 18:14:52 -0500

    update task component

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,25 +1,48 @@
-interface Props {
-    title: string,
-    description: string,
-    endDate: Date, 
-    currentState: boolean, 
-    notes: Array<string> 
+import { deleteTask, updateTask } from "../services/tasks.service";
 
+interface Props {
+  id: string;
+  title: string;
+  description: string;
+  endDate: Date;
+  currentState: boolean;
+  notes: Array<string>;
 }
 
-export default function Task({title, description, endDate, currentState, notes}:Props) {
+export default function Task({
+  id,
+  title,
+  description,
+  endDate,
+  currentState,
+  notes,
+}: Props) {
+  function handleComplete() {
+    updateTask(id, { currentState: true });
+  }
+
+  function handleDelete() {
+    deleteTask(id);
+  }
 
   return (
-
     <article>
-        <h3>{title} </h3>
-        <p>{description} </p>
-        <div><span>{endDate.toLocaleDateString("es-ES") }</span> <span>{currentState ? "Concluida" : "Pendiente"}</span></div> 
-        <ul>
-            {notes.map((note, index) => <li key={index} >{note}</li>)} 
-        </ul>
-        <button>editar</button>
-        <button>eliminar</button>
+      <h3>{title} </h3>
+      <p>{description} </p>
+      <div>
+        <span>{new Date(endDate).toLocaleDateString("es-ES")}</span>{" "}
+        <span>{currentState ? "Concluida" : "Pendiente"}</span>
+      </div>
+      <ul>
+        {notes.map((note, index) => (
+          <li key={index}>{note}</li>
+        ))}
+      </ul>
+      <button>editar</button>
+      <button onClick={handleDelete}>eliminar</button>
+      {currentState ? null : (
+        <button onClick={handleComplete}>completar</button>
+      )}
     </article>
-  )
+  );
 }
