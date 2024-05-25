2fd46f76dfd43dd569510f6e77e90d09b9233e2e
Norlys Castañeda <castaneda30@hotmail.com>
2024-05-25 18:15:53 -0500

    update tasks page

--- src/pages/Tasks.tsx
+++ src/pages/Tasks.tsx
@@ -1,29 +1,10 @@
-import Task from "../components/Task";
+import List from "../components/List";
 
-const listaTareas = [
-  {
-    id: 1,
-    title: "aprender react",
-    description: "revisar documentacion",
-    endDate: new Date(),
-    currentState: false,
-    notes: ["punto1", "punto2"],
-  },
-  {
-    id: 2,
-    title: "aprender Java",
-    description: "reunion",
-    endDate: new Date(),
-    currentState: false,
-    notes: ["punto4", "punto5"],
-  },
-];
 export default function Tasks() {
   return (
-    <section>
-      {listaTareas.map((tarea) => (
-        <Task {...tarea} key={tarea.id} />
-      ))}
-    </section>
+    <main>
+      <h1>To Do</h1>
+      <List />
+    </main>
   );
 }
