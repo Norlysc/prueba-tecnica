03ac7b047d3f80646763c2b70244c35e665ba9ad
Norlys Castañeda <castaneda30@hotmail.com>
2024-05-26 16:09:31 -0500

    update details page

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,8 +1,10 @@
-import { Button, IconButton } from "@mui/material";
-import { deleteTask, updateTask } from "../services/tasks.service";
-import DeleteIcon from "@mui/icons-material/Delete";
-import { useState } from "react";
+import { Box, Button, Chip, Typography } from "@mui/material";
+import { useContext, useState } from "react";
 import EditModal from "./EditModal";
+import { TasksContext } from "../contexts/TasksContext";
+import { useNavigate } from "react-router-dom";
+import { ROUTES } from "../constants/routes";
+import { stateColors } from "./TaskPreview";
 
 export default function Task({
   id,
@@ -12,7 +14,9 @@ export default function Task({
   currentState,
   notes,
 }) {
+  const navigate = useNavigate();
   const [open, setOpen] = useState(false);
+  const { handleDelete } = useContext(TasksContext);
 
   function handleOpen() {
     setOpen(true);
@@ -22,39 +26,78 @@ export default function Task({
     setOpen(false);
   }
 
-  function handleComplete() {
-    updateTask(id, { currentState: "terminada" });
-  }
-
-  function handleDelete() {
-    deleteTask(id);
-  }
-
   return (
-    <article>
-      <h3>{title} </h3>
-      <p>{description} </p>
-      <div>
-        <span>{new Date(endDate).toLocaleDateString("es-ES")}</span>{" "}
-        <span>{currentState}</span>
-      </div>
+    <Box component="article" maxWidth={"700px"}>
+      <Typography
+        variant="h1"
+        sx={{
+          fontSize: {
+            xs: "2rem",
+            sm: "2.5rem",
+            md: "3.5rem",
+          },
+          mb: "12px",
+          textAlign: "center",
+        }}
+      >
+        {title}
+      </Typography>
+      <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+        <Typography
+          variant="body1"
+          sx={{
+            fontSize: {
+              sx: "1rem",
+              md: "1.25rem",
+            },
+          }}
+        >
+          {new Date(endDate).toLocaleDateString("es-ES")}
+        </Typography>
+        <Chip
+          color={stateColors[currentState]}
+          label={currentState}
+          sx={{ textTransform: "capitalize" }}
+        />
+      </Box>
+      <Typography
+        variant="body1"
+        sx={{
+          my: "1.5rem",
+          fontSize: {
+            xs: "1rem",
+            sm: "1.5rem",
+            md: "2rem",
+          },
+        }}
+      >
+        {description}
+      </Typography>
       <ul>
         {notes.map((note, index) => (
           <li key={index}>{note}</li>
         ))}
       </ul>
-      <Button onClick={handleOpen}>editar</Button>
-      <IconButton onClick={handleDelete}>
-        <DeleteIcon sx={{ color: "red" }} />
-      </IconButton>
-      {currentState === "terminada" ? null : (
-        <button onClick={handleComplete}>completar</button>
-      )}
+      <Box sx={{ display: "flex", justifyContent: "flex-end", gap: "1rem" }}>
+        <Button onClick={handleOpen} variant="outlined">
+          Editar
+        </Button>
+        <Button
+          onClick={() => {
+            handleDelete({ id });
+            navigate(ROUTES.home);
+          }}
+          color="error"
+          variant="outlined"
+        >
+          Eliminar
+        </Button>
+      </Box>
       <EditModal
         open={open}
         handleClose={handleClose}
         task={{ id, title, description, endDate, currentState, notes }}
       />
-    </article>
+    </Box>
   );
 }
--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,5 +1,31 @@
+import { useContext, useEffect } from "react";
+import Task from "../components/Task";
+import { Container } from "@mui/material";
+import { TasksContext } from "../contexts/TasksContext";
+import { useParams } from "react-router-dom";
+import Spinner from "../components/Spinner";
+
 export default function Details() {
+  const { id } = useParams();
+  const { task, isLoading, fetchOneTask, refetch } = useContext(TasksContext);
+
+  useEffect(() => {
+    fetchOneTask({ id });
+  }, [refetch]);
+
   return (
-    <div>Detalles de tarea</div>
-  )
+    <Container
+      sx={{
+        width: "100%",
+        height: "100dvh",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        margin: "auto",
+      }}
+    >
+      {isLoading || !task ? <Spinner /> : <Task {...task} />}
+    </Container>
+  );
 }
