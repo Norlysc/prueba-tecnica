100142808cef619bb3ff9b85c093d1e9e36acf7a
Norlys Castañeda <castaneda30@hotmail.com>
2024-05-26 16:13:33 -0500

    create Spinner comonent

--- src/Router.jsx
+++ src/Router.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Tasks from "./pages/Tasks";
 import { ROUTES } from "./constants/routes";
 import { Suspense, lazy } from "react";
+import { Toaster } from "sonner";
+import Spinner from "./components/Spinner";
 
 const Details = lazy(() => import("./pages/Details"));
 const NewTask = lazy(() => import("./pages/NewTask"));
@@ -9,10 +11,11 @@ const NewTask = lazy(() => import("./pages/NewTask"));
 export default function Router() {
   return (
     <BrowserRouter>
-      <Suspense fallback={<div>Cargando...</div>}>
+      <Toaster richColors />
+      <Suspense fallback={<Spinner />}>
         <Routes>
           <Route path={ROUTES.home} element={<Tasks />} />
-          <Route path={ROUTES.details} element={<Details />} />
+          <Route path={`${ROUTES.details}/:id`} element={<Details />} />
           <Route path={ROUTES.new_task} element={<NewTask />} />
         </Routes>
       </Suspense>
